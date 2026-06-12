7b08b66fd91af4f16ad7b526fb5cacfb9f1aab5c
mimz <git@mimz.fr>
2026-06-12 17:05:38 +0200

    P1-09: UUID regex case
    MatchMigration.isUuid(): regex utilisait [0-9a-f] qui rejette l hex majuscule. Ajout RegexOption.IGNORE_CASE.

--- src/commonMain/kotlin/com/scoreo/infrastructure/MatchMigration.kt
+++ src/commonMain/kotlin/com/scoreo/infrastructure/MatchMigration.kt
@@ -60,4 +60,4 @@ fun migrateMatchesJson(
 }
 
 internal fun isUuid(str: String): Boolean =
-    Regex("[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}").matches(str)
+    Regex("[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", RegexOption.IGNORE_CASE).matches(str)
